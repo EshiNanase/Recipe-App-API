711074aedddb248f81e2b705f91bce29aab177ff
Nanashi <alksndr.zln@gmail.com>
2023-01-06 21:32:44 +0300

    Added command wait_for_database

--- app/core/management/commands/wait_for_database.py
+++ app/core/management/commands/wait_for_database.py
@@ -3,9 +3,22 @@ Django Command Waiting for Database to start
 """
 
 from django.core.management import BaseCommand
+import time
+from psycopg2 import OperationalError as PostgresError
+from django.db.utils import OperationalError
 
 
 class Command(BaseCommand):
 
-    def handle(self, *args, **kwargs):
-        pass
+    def handle(self, *args, **options):
+        """"""
+        self.stdout.write('Waiting for database...')
+        db_up = False
+        while db_up is False:
+            try:
+                self.check(databases=['default'])
+                db_up = True
+            except (PostgresError, OperationalError):
+                self.stdout.write('Database is not availible')
+                time.sleep(1)
+        self.stdout.write(self.style.SUCCESS('Database has started!'))
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -1,15 +1,28 @@
-from django.test import TestCase, SimpleTestCase
+from django.test import SimpleTestCase
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from unittest.mock import patch
 from psycopg2 import OperationalError as PostgresError
-from app.core.management.commands.wait_for_database import Command
 
 
-@patch('core.management.wait_for_db.Command.check')
+@patch('core.management.commands.wait_for_database.Command.check')
 class CommandTests(SimpleTestCase):
 
-    def test_wait_for_database(self, patched_check):
+    def test_wait_for_database_ready(self, patched_check):
+        # When this test is called, we just return bold bool
         patched_check.return_value = True
+
+        # Tests that command can be called
         call_command('wait_for_database')
-        patched_check.assert_called_once_with(database=['default'])
+
+        # check if method Command.check is called with these parameters
+        patched_check.assert_called_once_with(databases=['default'])
+
+    @patch('time.sleep')
+    def test_wait_for_database_delay(self, patched_check, patched_sleep):
+        """Test waiting when database delays its start (Operational Error)"""
+        patched_check.side_effect = [PostgresError] * 2 + [OperationalError] * 3 + [True]
+
+        call_command('wait_for_database')
+        self.assertEqual(6, patched_check.call_count)
+        patched_check.assert_called_with(databases=['default'])
