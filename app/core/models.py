7d6840bbe5e741b974df00e09553ace7468f4983
Nanashi <alksndr.zln@gmail.com>
2023-01-07 21:28:13 +0300

    Added superuser and fixed email addresses

--- app/core/models.py
+++ app/core/models.py
@@ -4,17 +4,19 @@ Models for Database
 
 from django.db import models
 from django.contrib.auth.models import (
-    AbstractUser, BaseUserManager, PermissionsMixin
+    AbstractBaseUser, BaseUserManager, PermissionsMixin
 )
 
 
 class UserManager(BaseUserManager):
     """Manager fo users"""
 
-    def create_user(self, email, name, password=None, **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
         """Creates a user"""
-        user = self.create(
-            email=email,
+        if not email:
+            raise ValueError('Email was not provided')
+        user = self.model(
+            email=self.normalize_email(email),
             **extra_fields
         )
         user.set_password(password)
@@ -22,10 +24,22 @@ class UserManager(BaseUserManager):
 
         return user
 
+    def create_superuser(self, email, password=None, **extra_fields):
+        user = self.model(
+            email=self.normalize_email(email),
+            **extra_fields
+        )
+        user.set_password(password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
 
-class User(AbstractUser, PermissionsMixin):
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
     """User in the system"""
-    email = models.EmailField(max_length=255, unique=True),
+    email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,19 +1,40 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
-from django.core.management import call_command
-from unittest.mock import patch
+import random
 
 
-@patch('core.management.commands.send_email.Command')
 class ModelTests(TestCase):
     """Testing models"""
 
-    def test_create_user_with_email(self, patched_send_email):
+    def test_create_user_with_email(self):
         email = "test@gmail.com"
         password = "123"
 
-        user = get_user_model().objects.create(email=email, password=password)
+        user = get_user_model().objects.create_user(email=email, password=password)
 
-        call_command('send_email')
         self.assertEqual(user.email, email)
-        self.assertEqual(user.password, password)
+        self.assertTrue(user.check_password(password))
+
+    def test_normalize_email(self):
+        """Test email is normalized for new users"""
+        sample_emails = [
+            ('test@EXAMPLE.com', 'test@example.com'),
+            ('TEST@EXAMPLE.COM', 'TEST@example.com'),
+            ('Test@exAMPLE.Com', 'Test@example.com')
+        ]
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email=email, password='123')
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_errors(self):
+        """Test creating a user will raise ValueError"""
+        with self.assertRaises(ValueError):
+            user = get_user_model().objects.create_user(email='', password='123')
+
+    def test_create_superuser(self):
+        """Test creating a superuser"""
+        user = get_user_model().objects.create_superuser(
+            email='test@example.com', password='123'
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
